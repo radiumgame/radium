8834fba855c96a29bf67d3630d3ac28cf1a35ce3
LandonHarter <landonharter@outlook.com>
2022-03-26 13:00:27 -0400

    Basic API functionality

---
+++ src/main/java/Integration/API/API.java
@@ -0,0 +1,48 @@
+package Integration.API;
+
+import RadiumEditor.Console;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class API {
+
+    protected API() {}
+
+    public static JSONObject Get(String request) {
+        try {
+            URL url = new URL("http://localhost:3000/packages");
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            int responsecode = conn.getResponseCode();
+
+            if (responsecode != 200) {
+                Console.Error("INVALID API CALL RESPONSE: " + responsecode);
+            } else {
+                String inline = "";
+                Scanner scanner = new Scanner(url.openStream());
+
+                while (scanner.hasNext()) {
+                    inline += scanner.nextLine();
+                }
+                scanner.close();
+
+                JSONParser parse = new JSONParser();
+                JSONObject data_obj = (JSONObject) parse.parse(inline);
+
+                return data_obj;
+            }
+        } catch (Exception e) {
+            Console.Error(e);
+        }
+
+        return null;
+    }
+
+}
